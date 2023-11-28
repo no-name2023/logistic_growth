8192f03d3ee89c0071967c169893054dabadda7a
zoe-george <150153182+zoe-george@users.noreply.github.com>
2023-11-28 15:38:29 +0000

    Update Graphs.R

--- Graphs.R
+++ Graphs.R
@@ -99,40 +99,40 @@ N_4980_logistic
 
 #Script to make a graph comparing the exponential and logistic growth curves
 
-# Loading the packages
+#Loading the packages
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("ggplot2")) install.packages("ggplot2")
 
-# Function to calculate logistic growth
+#Function to calculate logistic growth
 logistic_growth <- function(t, N0, r, K) {
   return(K / (1 + ((K - N0) / N0) * exp(-r * t)))
 }
 
-#function to calculate exponential growth 
+#Function to calculate exponential growth 
 exponential_growth <- function(t, N0, r) {
   return(N0 * exp(r * t))
 }
 
-# Parameter estimates
+# My parameter estimates
 N0 <- 6.903e+00
 r <- 9.990e-03
 K <- 5.979e+10
 
-# Generate time values
+#Generate time values
 t_values <- seq(0, 4980, by = 100)
 
-# Calculate population sizes for exponential and logistic growth
+#Calculate population sizes for exponential and logistic growth
 N_exp <- N0 * exp(r * t_values)
 N_logistic <- logistic_growth(t_values, N0, r, K)
 
-# Create a data frame for plotting
+#Create a data frame for plotting
 data_plot <- data.frame(
   Time = rep(t_values, 2),
   Population = c(N_exp, N_logistic),
   Model = rep(c("Exponential", "Logistic"), each = length(t_values))
 )
 
-# Plotting
+#Plotting the graph
 library(ggplot2)
 
 ggplot(data_plot, aes(x = Time, y = Population, color = Model)) +
